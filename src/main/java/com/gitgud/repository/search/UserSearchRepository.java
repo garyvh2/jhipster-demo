9052af427aec58993cb114d6893c270ed7514826
Gary Valverde Hampton <garyhampton.12@gmail.com>
2019-01-29 23:44:42 -0600

    Initial application generated by JHipster-5.8.1

---
+++ src/main/java/com/gitgud/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.gitgud.repository.search;
+
+import com.gitgud.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, String> {
+}
